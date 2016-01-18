5a133453be0863505bbcd35a5be9af3db8f5b8b5
meejah <meejah@meejah.ca>
2016-01-18 08:21:23 -0700

    fix up progress interface, and usage

--- src/allmydata/util/progress.py
+++ src/allmydata/util/progress.py
@@ -7,34 +7,23 @@ Ties in with the "consumer" module also
 from allmydata.interfaces import IProgress
 from zope.interface import implementer
 
-@implementer(IProgress)
-class AbsoluteProgress(object):
-    """
-    Merely remembers and gives back the same value as the last call to set_value
-    """
-
-    def __init__(self):
-        self._value = 0.0
-
-    def set_progress(self, value):
-        self._value = value
-
-    @property
-    def progress(self):
-        return self._value
-
 
 @implementer(IProgress)
-class PercentProgress(AbsoluteProgress):
+class PercentProgress(object):
     """
     Represents progress as a percentage, from 0.0 to 100.0
     """
 
     def __init__(self, total_size=None):
-        super(PercentProgress, self).__init__()
+        self._value = 0.0
         self.set_progress_total(total_size)
 
+    def set_progress(self, value):
+        "IProgress API"
+        self._value = value
+
     def set_progress_total(self, size):
+        "IProgress API"
         if size is not None:
             size = float(size)
         self._total_size = size
@@ -42,7 +31,7 @@ class PercentProgress(AbsoluteProgress):
     @property
     def progress(self):
         if self._total_size is None:
-            return 0
+            return 0  # or 1.0?
         if self._total_size <= 0.0:
             return 0
         return (self._value / self._total_size) * 100.0
