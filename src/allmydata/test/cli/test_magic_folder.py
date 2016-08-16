5e0342e99f3cbb43deb631b3d36ee79dec7e865e
meejah <meejah@meejah.ca>
2016-08-16 17:37:58 -0600

    lots more workie

--- src/allmydata/test/cli/test_magic_folder.py
+++ src/allmydata/test/cli/test_magic_folder.py
@@ -119,7 +119,7 @@ class MagicFolderCLITestMixin(CLITestMixin, GridTestMixin):
             d = self.magicfolder.finish()
             print("waiting for finish", d, self.magicfolder.uploader._turn_delay)
             # if we're shutting down due to error, we need a double-advance on the uploader (why??)
-            #self.magicfolder.uploader._clock.advance(self.magicfolder.uploader.scan_interval + self.magicfolder.uploader._turn_delay + 1)
+            self.magicfolder.uploader._clock.advance(self.magicfolder.uploader.scan_interval + self.magicfolder.uploader._turn_delay + 1)
             self.magicfolder.uploader._clock.advance(self.magicfolder.uploader._turn_delay + 1)
             self.magicfolder.downloader._clock.advance(self.magicfolder.downloader.scan_interval + 1)
             print("advanced both")
