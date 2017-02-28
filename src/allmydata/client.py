60fbca7a09a60add74b7da948e7db7ae937ee76d
meejah <meejah@meejah.ca>
2017-02-28 21:44:05 +0000

    WIP: more async-refactoring

--- src/allmydata/client.py
+++ src/allmydata/client.py
@@ -396,8 +396,14 @@ class _Client(node.Node, pollmixin.PollMixin):
         self.storage_server = ss
         self.add_service(ss)
 
-        self.accountant = ss.get_accountant()
-        self.accountant.set_expiration_policy(expiration_policy)
+        dbfile = os.path.join(storedir, "leasedb.sqlite")
+        statefile = os.path.join(storedir, "leasedb_crawler.state")
+        dbfile = os.path.join(self.storedir, "leasedb.sqlite")
+        statefile = os.path.join(self.storedir, "leasedb_crawler.state")
+        #self.accountant = create_accountant(ss, dbfile, statefile)
+        self.accountant = Accountant(self, dbfile, statefile)
+        self.accountant.set_expiration_policy(expiration_policy)  # pass to create_accountant?
+        self.accountant.setServiceParent(self)#ss)
 
         anonymous_account = self.accountant.get_anonymous_account()
         anonymous_account_furlfile = os.path.join(self.basedir, "private", "storage.furl").encode(get_filesystem_encoding())
