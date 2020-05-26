4eb132584590a044cee0e512adab07ae6a810cf3
meejah <meejah@meejah.ca>
2020-05-26 10:58:19 -0600

    in-progress haxxoring

--- integration/test_testing.py
+++ integration/test_testing.py
@@ -10,9 +10,6 @@ def test_retrieve_cap():
     WebUI Fake can serve a read-capability back
     """
 
-    root = create_fake_tahoe_root(
-        capabilities=[
-            "URI:CHK:7temc4rarxsffz4zrvze3feifi:7rclszi4oz42szvs7ntoimkfj52527ng3gttvqzdo7hr4duwtgcq:1:5:353911",
-        ]
-    )
+    root = create_fake_tahoe_root()
     print(root)
+
--- src/allmydata/testing/web.py
+++ src/allmydata/testing/web.py
@@ -39,7 +39,63 @@ class _FakeCapability(object):
     data=attr.ib()
 
 
-def create_fake_capability(cap):
+# XXX want to make all kinds of caps, like
+# URI:CHK:... URI:DIR2:... etc
+
+import allmydata.uri
+KNOWN_CAPABILITES = [
+    getattr(allmydata.uri, t).BASE_STRING
+    for t in dir(allmydata.uri)
+    if hasattr(getattr(allmydata.uri, t), 'BASE_STRING')
+]
+
+
+from allmydata.immutable.upload import BaseUploadable, IUplodable
+from os import urandom
+
+@implementer(IUploadable)
+class DataUploadable(BaseUploadable):
+    # Base gives us:
+    # set_upload_status
+    # set_default_encoding_parameters
+    # get_all_encoding_parameters
+
+    def __init__(self, data, key=None):
+        self._data = data
+        self._where = 0
+        self._key = key if key is not None else urandom(16)
+
+    def get_encryption_key(self):
+        return self._key
+
+    def get_size(self):
+        return len(self._data)
+
+    @inlineCallbacks
+    def read(self, amount):
+        data = self._data[self._where : self._where + amount]
+        self._where += amount
+        returnValue(data)
+
+    def close(self):
+        pass
+
+@inlineCallbacks
+def create_fake_capability(kind, data):
+    assert kind in KNOWN_CAPABILITIES
+
+    # XXX to use a allmydata.immutable.upload.CHKUploader directly,
+    # we'd need to instantiate:
+    uploader = CHKUploader(storage_broker, secret_holder, progress=None, reactor=None)
+    uploadable = DataUploadable(data)
+    encrypted_uploadable = EncryptAnUploadable(uploadable)
+
+    results = yield uploader.start(encrypted_uploadable)
+
+
+    # allmydata.immutable.upload.Uploader is a Service that 'does an upload'?
+    # has def upload(uploadable)
+
     return _FakeCapability(
         data=u"fake data for capability {}".format(cap)
     )
